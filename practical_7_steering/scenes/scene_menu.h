9e26753ea8902e50424887ac31b4311a1b704373
DESKTOP-H24B8E7\grant <40218994@live.napier.ac.uk>
2018-03-01 15:20:54 +0000

    AI: Finished steering

---
+++ practical_7_steering/scenes/scene_menu.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "engine.h"
+
+class MenuScene : public Scene {
+public:
+  MenuScene() = default;
+  ~MenuScene() = default;
+
+  void Load() override;
+  void Update(const double& dt) override;
+};
